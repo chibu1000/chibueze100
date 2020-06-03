044e9cd820f8b29ab9b4c87c342d318b73b72301
chibueze <chibuezenkwocha@gmail.com>
2020-06-03 20:05:38 +0100

    group task commit

---
+++ HNG-Script-master/chibueze-nkwocha.py
@@ -0,0 +1,6 @@
+name = "Chibueze Nkwocha"
+id = "HNG-03305"
+language = "python"
+email = "chibuezenkwocha@gmail.com"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
